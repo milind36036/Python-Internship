62950da9618079e54d4b458a87a4f4ba60babf0a
Milind Bhatnagar <milind36036@gmail.com>
2026-06-29 12:14:01 +0530

    Completed Python Internship Day 1

---
+++ Day01/hello.py
@@ -0,0 +1,10 @@
+print("=" * 40)
+print("🚀 Python Internship - Day 1")
+print("=" * 40)
+
+name = input("Enter your name: ")
+college = input("Enter your college: ")
+
+print("\nHello,", name)
+print("College:", college)
+print("Welcome to your Python Internship!")
